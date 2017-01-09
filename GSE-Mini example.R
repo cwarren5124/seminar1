dad726ce9b4c8808a1d48cf5e1c6a8864be62580
Cassia Warren <cassia.warren@gmail.com>
2017-01-09 15:24:05 -0800

    Done in R

---
+++ GSE-Mini example.R
@@ -0,0 +1,15 @@
+prDat <- read.table("GSE4051_MINI.tsv", header = TRUE, row.names = 1)
+prDat
+
+
+getwd()      # is working directory what you think it should be?
+list.files() # do see GSE4051_MINI.txt sitting there?!?
+setwd()      # use with moderation
+
+str(prDat)
+
+#subset
+weeDat <- subset(prDat,poisonFang > 7.5)
+
+#
+
---
+++ Packages.R
@@ -0,0 +1,12 @@
+#packages
+
+# data.frames
+install.packages("gapminder")
+library(gapminder)
+
+install.packages("tidyverse")
+library(tidyverse)
+
+#subsetting
+install.packages("dplyr")
+library(dplyr)
